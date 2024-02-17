c6b9c428ddc4f3b6d9e8534aa2c14613d5332327
Newton <u1804090@student.cuet.ac.bd>
2024-02-17 17:51:01 +0600

    Toggled the visibility of the contact form

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 
-        document.addEventListener('DOMContentLoaded', function () {
-            const contactButton = document.querySelector('a[href="#contact"]');
-            const contactForm = document.getElementById('contactForm');
-    
-            contactButton.addEventListener('click', function (event) {
-                event.preventDefault(); 
+    document.addEventListener('DOMContentLoaded', function () {
+        const contactButton = document.querySelector('a[href="#contact"]');
+        const contactForm = document.getElementById('contactForm');
+        let isContactFormVisible = false;
+
+        contactButton.addEventListener('click', function (event) {
+            event.preventDefault(); 
+            if (!isContactFormVisible) {
                 contactForm.style.display = 'block';
-               
                 contactForm.scrollIntoView({ behavior: 'smooth' });
-            });
+            } else {
+                contactForm.style.display = 'none';
+            }
+            isContactFormVisible = !isContactFormVisible;
         });
-  
+    });
+
